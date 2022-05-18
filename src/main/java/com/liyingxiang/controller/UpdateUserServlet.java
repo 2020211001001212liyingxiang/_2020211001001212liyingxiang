c2f16a9d6bdd6536637712092d4ba43385a85269
2020211001001212liyingxiang <1582983153@qq.com>
2022-05-18 17:02:30 +0800

    LAB-1

--- src/main/java/com/liyingxiang/controller/UpdateUserServlet.java
+++ src/main/java/com/liyingxiang/controller/UpdateUserServlet.java
@@ -1,61 +1,66 @@
 package com.liyingxiang.controller;
 
-        import com.liyingxiang.dao.UserDao;
-        import com.liyingxiang.model.User;
-        import jakarta.servlet.*;
-        import jakarta.servlet.http.*;
-        import jakarta.servlet.annotation.*;
+import com.liyingxiang.dao.UserDao;
+import com.liyingxiang.model.User;
 
-        import java.io.IOException;
-        import java.sql.Connection;
-        import java.sql.Date;
-        import java.sql.SQLException;
-        import java.text.ParseException;
-        import java.text.SimpleDateFormat;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
 
 @WebServlet(name = "UpdateUserServlet", value = "/updateUser")
 public class UpdateUserServlet extends HttpServlet {
+    Connection conn = null;
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        conn = (Connection) getServletContext().getAttribute("conn");
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("WEB-INF/views/updateUser.jsp").forward(request, response);
+        //forward to updateUser.jsp
+        request.getRequestDispatcher("WEB-INF/views/updateUser.jsp").forward(request,response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //get all(6) request parameters
         String id = request.getParameter("id");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String email = request.getParameter("email");
         String gender = request.getParameter("gender");
-        String birthday = request.getParameter("birthday");
-
+        String birthdate = request.getParameter("birthdate");
+        //create an object of User Model
         User user = new User();
-        user.setId(Integer.valueOf(id));
+        //set all 6 values into User Model
+        user.setId(Integer.parseInt(id));
         user.setUsername(username);
         user.setPassword(password);
         user.setEmail(email);
         user.setGender(gender);
-        // 这里的Date.valueOf()是java.sql.Date类的方法，而User中的Date类是java.util.Date
-        // java.sql.Date类是继承java.util.Date类的，等于这里使用了向上转型
-        user.setBirthDate(Date.valueOf(birthday));
-
+        //这里的Date是java.sql.Date,继承于java.util.Date,可以进行强转
+        user.setBirthdate(Date.valueOf(birthdate));
+        //create an object of UserDao
         UserDao userDao = new UserDao();
-        ServletContext application = getServletContext();
-        Connection conn = (Connection) application.getAttribute("conn");
-        int num = 0;
+        //call updateUser() in UserDao
         try {
-            num = userDao.updateUser(conn, user);
+            if (userDao.updateUser(conn,user) == 1){
+                //kill the old session
+                request.getSession(false).invalidate();//kill right now
+                //create a new session
+                HttpSession session = request.getSession();
+                session.setMaxInactiveInterval(10*60);
+                session.setAttribute("user",user);
+                //forward to userInfo.jsp
+                request.getRequestDispatcher("WEB-INF/views/userInfo.jsp").forward(request,response);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-
-        if (num != 0){
-            HttpSession session = request.getSession();
-            session.setAttribute("user", user);
-            request.getRequestDispatcher("WEB-INF/views/userInfo.jsp").forward(request, response);
-        }else {
-            request.setAttribute("message", "Update fail!");
-            request.getRequestDispatcher("WEB-INF/views/updateUser.jsp").forward(request, response);
         }
     }
 }
